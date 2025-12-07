49af8ee97b4006811d2a93ef9666cb11af8a2753
Angel Dominguez Plata <angeldomp49@gmail.com>
2025-12-07 10:25:56 -0600

    fix(Operations): Removed a bug in the getLeafValue into the StringLeaf

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -47,7 +47,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.makechtec.software"
             artifactId = "json_tree"
-            version = "3.1.4"
+            version = "3.1.5"
 
             from(components["java"])
         }
